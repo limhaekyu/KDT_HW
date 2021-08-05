29f4d321a062dfeb7fcc7cdd145b6f4323270688
limhaekyu <limk9500@gmail.com>
2021-08-05 21:42:04 +0900

    feat: refectoring
    1. 입력 받은 값을 숫자로 변경할때 사용한 +를 Number()을 사용하는 것으로
       변경
    2. for문에서 length-1을 length로 변경하고 가장 비싼음식을 data의
       마지막에 위치하게 변경후 검사

--- mini_project2/main.js
+++ mini_project2/main.js
@@ -4,17 +4,17 @@ const data = [
     { name: '아이스크림', price: 1000 },
     { name: '컵라면', price: 1600 },
     { name: '볼펜', price: 2500 },
-    { name: '아메리카노', price: 4000 },
+    { name: '껌', price: 500 },
     { name: '과자', price: 3000 },
     { name: '탄산수', price: 1200 },
     { name: '떡볶이', price: 3500 },
     { name: '노트', price: 1500 },
-    { name: '껌', price: 500 }
+    { name: '아메리카노', price: 4000 }
 ];
 
 // 사용자 입력 받기
 const line = prompt('최대 금액을 입력해주세요.');
-const amount = +line;
+const amount = Number(line);
 
 // 주어진 금액으로 살 수 있는 가장 비싼 상품을 구함
 const item = getItemByAmount(data, amount);
@@ -33,8 +33,8 @@ function getItemByAmount(array, value) {
   // product 객체 생성
   let temp = 0;
   // 현재의 product의 가격을 담을 변수 생성
-  for (let i = 0; i < array.length-1; i++){
-    if( array[i].price <= value){
+  for (let i = 0; i < array.length; i++){
+    if(array[i].price <= value){
       if(temp < array[i].price){
         // 이전의 상품의 가격과 현재의 상품의 가격을 비교
         temp = array[i].price;
